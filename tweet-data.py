d10fba72ad3c7a1c305f6f7d2be996c292555005
glassworks-projects <ryan.michael.glassman@gmail.com>
2019-12-05 15:17:09 -0500

    added cli

--- tweet-data.py
+++ tweet-data.py
@@ -2,34 +2,48 @@ import tweepy
 from wordcloud import WordCloud
 import re
 import matplotlib.pyplot as plt
+import emoji
+import click
 
 # generate word cloud 
 def wc(s) : 
     wordcloud = WordCloud().generate(s)
-    # Display the generated image:
-    # the matplotlib way:
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
-    # plt.figure()
     plt.show()
+
+def extract_emojis(str):
+  return ''.join(c for c in str if c in emoji.UNICODE_EMOJI)
 
 def remove_urls(r : tweepy.cursor.ItemIterator):
     return " ".join([re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", tweet.full_text) 
                  for tweet in r
                     if "RT @" not in tweet.full_text])
 
-client_id = "ZqT9qiED8ogLYdVLpK2nxVoCW"
-client_secret = "3skyBKuf9RJndvfcszRmKWZaRuNsxHAxpDQeKws4a2Il9B1w3S"
-access_key= '3343168235-39yNUvXmStC59DxI1rDXUGYvukbnX3xV9qYiB5p'
-access_secret = '3ynInkKMBBbaBatHyq5UgqQEwfbq1jPZh2xUMpvxFC7Ar'
+@click.command(context_settings={'help_option_names':['-h','--help']})
+@click.argument('username')
+@click.argument('tweet_count')
 
-auth = tweepy.OAuthHandler(client_id, client_secret)
-auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
+def data_viz(username, tweet_count) :
+    try :
+        count = int(tweet_count)
+    except ValueError :
+        print("Invalid tweet count: {}".format(tweet_count))
+        return
 
-r = tweepy.Cursor(api.user_timeline, id='realdonaldtrump', tweet_mode='extended').items(500)
-cloud_string = remove_urls(r)
+    client_id = "ZqT9qiED8ogLYdVLpK2nxVoCW"
+    client_secret = "3skyBKuf9RJndvfcszRmKWZaRuNsxHAxpDQeKws4a2Il9B1w3S"
+    access_key= '3343168235-39yNUvXmStC59DxI1rDXUGYvukbnX3xV9qYiB5p'
+    access_secret = '3ynInkKMBBbaBatHyq5UgqQEwfbq1jPZh2xUMpvxFC7Ar'
 
-wc(cloud_string)
+    auth = tweepy.OAuthHandler(client_id, client_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
 
-# testing commit
+    r = tweepy.Cursor(api.user_timeline, id=username, tweet_mode='extended').items(count)
+    cloud_string = remove_urls(r)
+
+    wc(cloud_string)
+
+if __name__ == '__main__' :
+    data_viz()
